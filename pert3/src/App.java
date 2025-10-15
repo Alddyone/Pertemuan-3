77e545c88a70d880738cb308fecafc87d82cc581
Alddyone <shandikaaldino025@gmail.com>
2025-10-15 11:09:05 +0700

    Add files via upload

---
+++ pert3/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Mahasiswa mahasiswa1 = new Mahasiswa("Jossi", "IT", "4524210097", 3.51);
+        mahasiswa1.tampilData();
+    }
+}
---
+++ pert3/src/Mahasiswa.java
@@ -0,0 +1,21 @@
+public class Mahasiswa {
+    String npm;
+    String nama;  
+    String jurusan;
+    double ipk;
+
+    Mahasiswa(String paramNama,String paramJurusan , String paramNpm, double paramIpk ) {
+        nama = paramNama;
+        jurusan = paramJurusan;
+        npm = paramNpm;
+        ipk = paramIpk;
+    }
+
+    void tampilData() {
+        System.out.println("Nama     : " + nama);
+        System.out.println("Jurusan  : " + jurusan);
+        System.out.println("NPM      : " + npm);
+        System.out.println("IPK      : " + ipk); 
+    }
+
+} 
